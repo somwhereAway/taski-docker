6a4b8b4db316a2fc30b67f01ceed5aa205fbb3ee
Андрей Щапов <a.stshapov@gmail.com>
2023-11-15 00:08:13 +0700

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS", default='127.0.0.1, localhost').split(' ')
+    "ALLOWED_HOSTS", default='127.0.0.1 localhost').split(' ')
 
 
 # Application definition
